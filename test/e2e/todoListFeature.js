ec3a8730aec7dfe757ff07d95a19460545785e75
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-31 23:37:18 +0000

    Adds ability to edit tasks

--- test/e2e/todoListFeature.js
+++ test/e2e/todoListFeature.js
@@ -7,8 +7,18 @@ describe('Todo List', function() {
 
   it('can add tasks', function() {
     browser.get('http://localhost:8080');
-    element(by.model('todoCtrl.newTask')).sendKeys('Create a to do list');
+    element(by.model('todoCtrl.newTask')).sendKeys('Learn AngularJS');
     element(by.className('btn')).click();
-    expect(element(by.tagName('li')).getText()).toEqual('Create a to do list');
+    expect(element(by.tagName('li')).getText()).toEqual('Learn AngularJS');
+  });
+
+  it('allows tasks to be edited', function() {
+    browser.get('http://localhost:8080');
+    element(by.model("todoCtrl.newTask")).sendKeys('Learn AngularJS.');
+    element(by.buttonText("Add Task")).click();
+    element(by.tagName('span')).click();
+    element(by.model("task.text")).sendKeys(' Or not.');
+    element(by.buttonText('Save')).click();
+    expect(element(by.tagName('span')).getText()).toEqual('Learn AngularJS. Or not.');
   });
 });
